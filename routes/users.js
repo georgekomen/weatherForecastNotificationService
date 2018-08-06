188c6061a97301ae73cd4afd5b488c99d330ca9e
KOMEN <g.komen@carepay.co.ke>
2018-08-07 00:19:37 +0300

    find all and by query string

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,6 @@ var router = express.Router();
 var User = require('../Domain/User');
 var mongoose = require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-
 /* post user. */
 router.post('/', function(req, res, next) {
     const user1 = new User({
@@ -19,6 +13,21 @@ router.post('/', function(req, res, next) {
         console.log(res);
         res.status(200).json(res1);
     }).catch(error1=>res.status(500).json(error1));
+});
+
+/* GET user by name(param) */
+router.get('/', function(req, res, next) {
+  User.findOne({name:req.query.name}).exec().then(res1 => {
+      res.status(200).json(res1);
+  }).catch(err => {res.status(500).json(err)});
+});
+
+/* GET all users */
+router.get('/all', function(req, res, next) {
+   User.find().exec().then(res1 => {
+       console.log(res1);
+       res.status(200).json(res1);
+   });
 });
 
 module.exports = router;
