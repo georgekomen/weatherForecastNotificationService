eb12ca4e510152f572b7c46c2cd04c24b277d924
KOMEN <g.komen@carepay.co.ke>
2018-08-11 21:57:59 +0300

    fetch forecast per person

--- Service/weatherbit.js
+++ Service/weatherbit.js
@@ -1,14 +1,15 @@
 const request = require("request")
-exports.forecast = function() {
-    request.get({url: 'http://api.weatherbit.io/v2.0/forecast/daily', 
+exports.forecast = function(city, country) {
+     request.get({url: 'http://api.weatherbit.io/v2.0/forecast/daily', 
         qs: {
-            'city'      : 'Nakuru',
-            'country'   : 'Kenya',
+            'city'      : city,
+            'country'   : country,
             'key'  : '96799479978e4b6bbb2c9cffda8d3fd8',
             'days': 1
         }
     },
     function(err, response, body) {
-        console.log(err, body);
+        // console.log(err, body);
+        return body;
     });
 }
--- ServiceImpl/sendforecasts.js
+++ ServiceImpl/sendforecasts.js
@@ -3,13 +3,13 @@ var africastalking = require('../Service/africastalking');
 var User = require('../Domain/User');
 
 exports.sendforecasts = function(req, res, next) {
-    var weatherforecast = weatherapi.forecast();
-    console.log(weatherforecast);
     User.find().exec().then(res1 => {
-        let users = [];
-        users = res1;
-        users.forEach(res => {
-          console.log(res.country);
+        res1.forEach(res2 => {
+            if (res2.country != undefined && res2.country != null && 
+                res2.location != undefined && res2.location != null) {
+                    var weatherforecast = weatherapi.forecast(res2.location, res2.country);
+                    console.log("Haya basi: ", weatherforecast);
+                }
         });
        res.status(200).json(res1);
     });
