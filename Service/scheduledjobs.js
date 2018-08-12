606ac362a18b73ced14993bcea3c1d6b2e50929b
KOMEN <g.komen@carepay.co.ke>
2018-08-12 12:02:46 +0300

    change time schedule

--- Service/scheduledjobs.js
+++ Service/scheduledjobs.js
@@ -2,7 +2,7 @@ var schedule = require('node-schedule');
 var SendforecastI = require('../ServiceImpl/sendforecasts');
  
 exports.sendforecast = function() {
-    var sendforecast = schedule.scheduleJob('42 11 * * *', function() { //sec,min,hour,date,month,day
+    var sendforecast = schedule.scheduleJob('00 07 * * *', function() { //sec,min,hour,date,month,day
        SendforecastI.sendforecasts(null, null, null);
     });
 }
