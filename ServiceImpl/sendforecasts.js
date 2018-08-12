c94179a2610d639eec4c4d43333e3ec79ca5854e
KOMEN <g.komen@carepay.co.ke>
2018-08-12 11:43:31 +0300

    sending temp forecast

--- ServiceImpl/sendforecasts.js
+++ ServiceImpl/sendforecasts.js
@@ -8,9 +8,10 @@ exports.sendforecasts = function(req, res, next) {
             if (res2.country != undefined && res2.country != null && 
                 res2.location != undefined && res2.location != null &&
                 !isNaN(res2.phonenumber)) {
-                    weatherapi.forecast(res2.location, res2.country).then(weatherforecast => {
-                      africastalking.sendMessage(res2.phonenumber, "testing");
-                    });
+                    weatherapi.forecast(res2.location, res2.country).then(res3 => {
+                        var weatherforecast = JSON.parse(res3);
+                        africastalking.sendMessage(res2.phonenumber, `Hi ${res2.name}, Mobias Weather forecast for tommorow: temperature: ${weatherforecast.data[0].temp}`);
+                        });
                 }
         });
         if (res) {
